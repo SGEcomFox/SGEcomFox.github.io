394de43fa9e22529614238572eccf59bd6e794bb
SGEcomFox <98043407+SGEcomFox@users.noreply.github.com>
2025-08-03 14:22:17 +0200

    Updated Item Card

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ function buildTestItems(amount) {
     for(let i=0; i<amount; i++) {
         const newItem = $('<div>', {
             class: 'itemCard',
-            id : 'item'+i
+            id: 'item'+i
         });
         $('main').append(newItem);
         const newIcon = $('<img>', {
@@ -16,5 +16,19 @@ function buildTestItems(amount) {
             src: '/icons/sword.png'
         });
         newItem.append(newIcon)
+        const newItemName = $('<label>', {
+            class: 'itemName',
+            id: 'itemName'+i
+        });
+        newItemName.text('testItem');
+        newItem.append(newItemName);
+        const newItemCount = $('<label>', {
+            class: 'itemCount',
+            id: 'itemCount'+i
+        });
+        newItemCount.text(Math.floor(Math.random() * 99) + 1);
+        newItem.append(newItemCount);
+
+        
     }
 }
